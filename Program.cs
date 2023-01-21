9350c6b87d53d099b68cf3badb652de2c567710f
Kolavennu Sai Sreeram Kumar <79280855+6304284500@users.noreply.github.com>
2023-01-21 08:20:53 +0530

    Main Program Where all are called

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using LogicalProblems;
+public class program
+{
+    public static void Main(string[] args)
+    {
+        fibonacci  fibonacci = new fibonacci();
+        Perfect  perfect = new Perfect();
+        Reverse reverse = new Reverse();
+        Coupon coupon = new Coupon();
+        stop stop = new stop();
+       
+        fibonacci.a(); 
+        perfect.b();
+        reverse.d();
+        coupon.e ();
+        stop.f();
+    }
+}
