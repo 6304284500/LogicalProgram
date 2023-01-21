bcaf97f27f4b326695edc6f61eec78cf619a8acf
Kolavennu Sai Sreeram Kumar <79280855+6304284500@users.noreply.github.com>
2023-01-21 08:13:21 +0530

    Program for Fibonacci Series

---
+++ Fibonaaci.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProblems
+{
+
+    public class fibonacci
+    {
+        public void a()
+        {
+
+            {
+                int len = 0;
+                int a = 0, b = 1, c = 0;
+                Console.Write("{0} {1}", a, b);
+                for (int i = 2; i < len; i++)
+                {
+                    c = a + b;
+                    Console.Write(" {0}", c);
+                    a = b;
+                    b = c;
+                }
+            }
+        }
+    }
+}
