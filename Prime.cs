a0aebcf1309dc470fa5846374ad488826e5dbb81
Kolavennu Sai Sreeram Kumar <79280855+6304284500@users.noreply.github.com>
2023-01-21 08:16:37 +0530

    Program to find Prime Number

---
+++ Prime.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProblems
+{
+    public class Prime
+    {
+        public void c()
+        {
+            int n, i, m = 0, flag = 0;
+            Console.Write("Enter the Number to check Prime: ");
+            n = int.Parse(Console.ReadLine());
+            m = n / 2;
+            for (i = 2; i <= m; i++)
+            {
+                if (n % i == 0)
+                {
+                    Console.Write("Number is not Prime.");
+                    flag = 1;
+                    break;
+                }
+            }
+            if (flag == 0)
+                Console.Write("Number is Prime.");
+        }
+
+    }
+}
+
+
